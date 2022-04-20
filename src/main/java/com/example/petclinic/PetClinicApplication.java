aaa261deda1e4ab8cd6b719a6d1a6b360b1b2bd2
vildan-b <vildanb@gmx.de>
2022-04-20 17:50:36 +0200

    TestServlet

--- src/main/java/com/example/petclinic/PetClinicApplication.java
+++ src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -3,8 +3,11 @@ package com.example.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
+
+@ServletComponentScan
 @SpringBootApplication
 @ComponentScan("com.example.petclinic")
 @EnableConfigurationProperties(value=PetClinicProperties.class)
